a79eab5a9a59e63dfa8c13e2a88f3b420747b832
xavier-francois-dev <dev.xavier.francois@gmail.com>
2025-02-03 10:02:43 +0100

    Add auth setup: login & logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :tests
+  resource :session
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
